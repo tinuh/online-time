e39af98792bf355057fbfe10becf251818df821e
tinu4 <tinuvanapamula@gmail.com>
2021-04-24 14:07:45 -0400

    minor change

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import '../styles/style.css';
 
 //components
 import Button from "@material-ui/core/Button";
-import Modal from "@material-ui/core/modal";
+import Modal from "@material-ui/core/Modal";
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
